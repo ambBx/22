1bfc599e795f84aaa26d130f28dc746620bcf47b
cosmickitten <vasenko_boris@mail.ru>
2022-06-26 13:50:54 +0300

    add while

--- veritas.py
+++ veritas.py
@@ -1,25 +1,29 @@
 import sys
+answer = 'да'
+while answer == "да":
 
-a = input("введите а : ")
-b = input("введите б : ")
-a=str(a.lower())
-b=str(b.lower())
-if (a == 'и') or (a == '1') or (a=='true'):
-    a = True
-elif (a == 'л') or (a == '0') or (a=='false'):
-    a = False
-else:
-    print("Ошибка ввода А")
-    sys.exit()
-if (b == 'и') or (b == '1') or (b=='true'):
-    b = True
-elif (b == 'л') or (b == '0') or (b=='false'):
-    b = False
-else:
-    print("Ошибка ввода B")
-    sys.exit()
+    a = input("введите а : ")
+    b = input("введите б : ")
+    a=str(a.lower())
+    b=str(b.lower())
+    if (a == 'и') or (a == '1') or (a=='true'):
+        a = True
+    elif (a == 'л') or (a == '0') or (a=='false'):
+        a = False
+    else:
+        print("Ошибка ввода А")
+        sys.exit()
+    if (b == 'и') or (b == '1') or (b=='true'):
+        b = True
+    elif (b == 'л') or (b == '0') or (b=='false'):
+        b = False
+    else:
+        print("Ошибка ввода B")
+        sys.exit()
 
 
-print("A \t B \t AvB \t A&B \t не A \t не B")
-print(a,"\t",b,"\t",(a or b),"\t",(a and b),"\t",(not a),"\t",(not b)) 
+    print("A \t B \t AvB \t A&B \t не A \t не B")
+    print(a,"\t",b,"\t",(a or b),"\t",(a and b),"\t",(not a),"\t",(not b)) 
+    s=input("Продолжить да/нет?")
+    answer = s.lower()
 
